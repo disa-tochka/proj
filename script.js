618b5e2b5fabd2972708a7b171dfb53403d405d0
Георгий Ефременко <161922236+disa-tochka@users.noreply.github.com>
2024-05-18 16:38:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let btn = document.querySelector('.btn')
+let overlay = document.querySelector('.overlay')
+let btn_send = document.querySelector('.btn-send')
+let background = document.querySelector('.background')
+
+btn.addEventListener('click',function(){
+    overlay.style.display = 'flex'
+})
+
+btn_send.addEventListener('click',function(){
+    overlay.style.display = 'none'
+})
+
+background.addEventListener('click',function(){
+    overlay.style.display = 'none'
+})
+
 
